6c116cbd275e9a2263717ae5bed12e8598756621
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-05 13:44:15 -0500

    Added post

--- server.js
+++ server.js
@@ -11,11 +11,25 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
+  });
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
   });
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
   function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
