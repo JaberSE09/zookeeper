a9564f4769173c5d5fdb627bc2d01d840a5c8951
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-05 11:56:11 -0500

    Add Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -11,7 +12,7 @@ app.get('/api/animals', (req, res) => {
   });
 
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
   });
 
